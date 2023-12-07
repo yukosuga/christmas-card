715ea9fe9a80d9612b394dc7b260e8674dc06f47
Yuko <yuko.gerheim@gmail.com>
2023-12-07 21:43:42 +0100

    Modified padding

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ const text = document.querySelector("#text");
 const button = document.querySelector("#button");
 
 button.addEventListener("click", () => {
-  console.log("clicked");
   button.style.display = "none";
   text.style.display = "block";
   setTimeout(() => {
-    text.style.opacity = "1"; // Fade in the play text
+    text.style.opacity = "1";
   }, 50);
 });
