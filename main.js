de844683d02c4b1e56dd063a2295d53617650ec6
Yuko <yuko.gerheim@gmail.com>
2023-12-08 20:20:08 +0100

    Added audio

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const card = document.querySelector(".card");
 const text = document.querySelector("#text");
 const button = document.querySelector("#button");
+const audio = new Audio("christmas_card.mp3")
 
 button.addEventListener("click", () => {
   button.style.display = "none";
   text.style.display = "block";
+
+  audio.play();
+
   setTimeout(() => {
     text.style.opacity = "1";
-  }, 50);
+  }, 800);
 });
